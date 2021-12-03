669d894d94515578db981089fe5456076d9ccaec
andy123t <andy123t@163.com>
2021-12-03 16:08:01 +0800

    Update

---
+++ book-en/compile-pdflatex-bibtex.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#-> Get source filename
+find . -name "*.tex"|while read FileName;
+do 
+#--- Process FileName.tex ---
+echo "$FileName"
+echo "${FileName%.tex}"
+#-> Compile the main file
+pdflatex --synctex=-1 "$FileName"
+bibtex "${FileName%.tex}"
+pdflatex --synctex=-1 "$FileName"
+pdflatex --synctex=-1 "$FileName"
+#clear aux files
+rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
+done
+
