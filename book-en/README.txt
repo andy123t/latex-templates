73d82706315e342cb20a4d68cf925389e73d7b26
andy123t <andy123t@163.com>
2023-01-23 22:06:21 +0800

    update

--- book-en/README.txt
+++ book-en/README.txt
@@ -1,10 +1,10 @@
 	
-    book-en.tex
-    English Book Template
-    PDFLaTeX compilation
-	
-    book-cover-en.tex
-    English Book Template (Custom cover)
-    PDFLaTeX compilation
+  book-en.tex
+  English Book Template
+  PDFLaTeX compilation
+  
+  book-cover-en.tex
+  English Book Template (Custom cover)
+  PDFLaTeX compilation
 	
 	
--- paper-cn/makefiles.sh
+++ paper-cn/makefiles.sh
@@ -8,6 +8,8 @@ echo "$FileName"
 echo "${FileName%.tex}"
 #-> Compile the main file
 xelatex --synctex=-1 "$FileName"
+bibtex "${FileName%.tex}"
+xelatex --synctex=-1 "$FileName"
 xelatex --synctex=-1 "$FileName"
 #clear aux files
 rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
