e6fd1a0367e09a6b668d677b0b410c116851fc35
andy123t <andy123t@163.com>
2024-09-30 13:32:33 +0800

    update

---
+++ book-en/makefiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#-> Get source filename
+find . -maxdepth 1 -type f -name "*.tex" | while read FileName; do 
+#--- Process FileName.tex ---
+echo "$FileName"
+echo "${FileName%.tex}"
+#-> Compile the main file
+pdflatex --synctex=-1 "$FileName"
+bibtex "${FileName%.tex}"
+pdflatex --synctex=-1 "$FileName"
+pdflatex --synctex=-1 "$FileName"
+#clear aux files
+rm -r *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb 
+done
---
+++ faq/cleaner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Delete files with the specified extensions
+rm -f *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex\(busy\)
+
+rm -f *.fls *.xdv *.fdb_latexmk
